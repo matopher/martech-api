cc0b600005f0146ea829ec004e2ee0303f8950f0
Matt Woods <mattwoods21@gmail.com>
2018-02-11 23:49:04 -0600

    use seeds csv to populate mongoose database

--- seeds/seed.js
+++ seeds/seed.js
@@ -2,15 +2,41 @@
 const csv = require('csvtojson');
 const mongoose = require('mongoose');
 const Companies = require('../models/companies');
+require('dotenv').config();
 
 console.log('seeds 🙌');
 
+// Connect to MongoDB
+mongoose.connect(
+  `mongodb://${process.env.MONGO_USER}:${process.env.MONGO_PASS}@${
+    process.env.MONGO_HOST
+  }`
+);
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+  // we're connected!
+  console.log(`mongodb seed connection opened`);
+});
+
 function insertRecordIntoDatabase(record) {
   // Insert into mongo
-  const company = new Companies({ record });
-  console.log(company);
+
+  const company = new Companies({
+    category: record.category,
+    subCategory: record.subCategory,
+    company: record.company,
+    websiteDomain: record.websiteDomain
+  });
+  // console.log(company);
   company.save(function(err, company) {
     if (err) return console.error(err);
+    console.log(company);
+
+    // console.log(record.category);
+    // console.log(record.subCategory);
+    // console.log(record.company);
+    // console.log(record.websiteDomain);
   });
 }
 
