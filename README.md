ccb9a4ee30677ff3fc1fb818bbd8745dfafa5ac7
Matt Woods <mattwoods21@gmail.com>
2018-02-22 21:06:55 -0600

    added dump script to delete sample data and updated README

--- README.md
+++ README.md
@@ -1,8 +1,14 @@
 # martech-api
 
-# Load Seeds
+This is an API to access data including category, subcategory, domain, and company name from Scott Brinker's infamous Martech 5000 infographic.
+
+# Load Sample Data
 
 Start seeds using `npm run seeds`
+
+# Delete Sample Data
+
+Delete sample data using `npm run dump`
 
 # Run Project
 
--- seeds/seed.js
+++ seeds/seed.js
@@ -16,38 +16,46 @@ db.once('open', function() {
 
 function insertRecordIntoDatabase(record) {
   // Insert into mongo
-
   const company = new Companies({
     category: record.category,
     subCategory: record.subCategory,
     company: record.company,
     websiteDomain: record.websiteDomain
   });
-  // console.log(company);
   company.save(function(err, company) {
     if (err) return console.error(err);
     console.log(company);
-
-    // console.log(record.category);
-    // console.log(record.subCategory);
-    // console.log(record.company);
-    // console.log(record.websiteDomain);
   });
 }
 
-// Read the CSV File
-const records = [];
-const csvFilePath = './seeds/martech-landscape.csv';
-csv()
-  .fromFile(csvFilePath)
-  .on('json', jsonObj => {
-    // Pushes each CSV row to in memory array of records
-    records.push(jsonObj);
-  })
-  .on('done', error => {
-    console.log('end', error);
+async function loadData() {
+  // Read the CSV File
+  const records = [];
+  const csvFilePath = './seeds/martech-landscape.csv';
+  csv()
+    .fromFile(csvFilePath)
+    .on('json', jsonObj => {
+      // Pushes each CSV row to in memory array of records
+      records.push(jsonObj);
+    })
+    .on('done', error => {
+      console.log('end', error);
 
-    // Insert JSON into Database
-    records.forEach(record => insertRecordIntoDatabase(record));
-    console.log('done inserting');
-  });
+      // Insert JSON into Database
+      records.forEach(record => insertRecordIntoDatabase(record));
+      console.log('done inserting');
+    });
+}
+
+async function deleteData() {
+  console.log('😢😢 Goodbye Data...');
+  await Companies.remove();
+  console.log('Data Deleted. To load sample data, run\n\n\t npm run seeds\n\n');
+  process.exit();
+}
+
+if (process.argv.includes('--delete')) {
+  deleteData();
+} else {
+  loadData();
+}
