e3365057d3a34a7bbae60566d96edf3dcbfb2ba2
Matt Woods <mattwoods21@gmail.com>
2018-03-11 23:14:26 -0500

    update mongohost

--- config/prod.js
+++ config/prod.js
@@ -5,6 +5,6 @@ let ENV = {};
 ENV.port = process.env.PORT || 3000;
 ENV.mongoUser = process.env.MONGO_USER || `admin`;
 ENV.mongoPass = process.env.MONGO_PASS || `password`;
-ENV.mongoHost = process.env.MONGO_HOST || `localhost:3000`;
+ENV.mongoHost = process.env.MONGO_HOST || `ds111258.mlab.com:11258/matopher-restful-api`;
 
 module.exports = ENV;
