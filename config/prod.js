ae27f0d14df165a444859b30317a713d42feeadb
Matt Woods <mattwoods21@gmail.com>
2018-03-11 23:33:48 -0500

    updated config

--- config/prod.js
+++ config/prod.js
@@ -2,9 +2,9 @@ require('dotenv').config();
 
 let ENV = {};
 
-ENV.port = process.env.PORT || 3000;
-ENV.mongoUser = process.env.MONGO_USER || `admin`;
-ENV.mongoPass = process.env.MONGO_PASS || `password`;
-ENV.mongoHost = process.env.MONGO_HOST || `ds111258.mlab.com:11258/matopher-restful-api`;
+ENV.port = process.env.PORT;
+ENV.mongoUser = process.env.MONGO_USER;
+ENV.mongoPass = process.env.MONGO_PASS;
+ENV.mongoHost = process.env.MONGO_HOST;
 
 module.exports = ENV;
